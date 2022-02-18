2b6b752a7c57d6b2aa7053ab7db73e2abbf7deaf
hyungjujeon <gudwn1549@gmail.com>
2022-02-19 00:16:52 +0900

    feat: init springboot

---
+++ src/main/java/com/ipoallmine/ipo_reminder_web/IpoWebApplication.java
@@ -0,0 +1,13 @@
+package com.ipoallmine.ipo_reminder_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipoallmine/ipo_reminder_web/IpoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipoallmine.ipo_reminder_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
